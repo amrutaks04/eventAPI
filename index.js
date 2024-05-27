3c7443b0d908dce5fdba908c10e93f3e33c8a30c
amruta.ks <amruta.ks2022cse@sece.ac.in>
2024-05-27 12:32:57 +0530

    u

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ app.get('/req-event', async function (request, response) {
         });
     }
 });
-
 app.post('/add-eventdes', async function (request, response) {
     try {
         const newEventDes = await Eventdes.create({
@@ -92,6 +91,7 @@ app.post('/add-eventdes', async function (request, response) {
     }
 });
 
+// Route to get a detailed event by ID
 app.get('/eventdes/:id', async function (request, response) {
     try {
         const event = await Eventdes.findById(request.params.id);
