2aa20a87dbd5fccb0993524634caf2320a791811
amruta.ks <amruta.ks2022cse@sece.ac.in>
2024-06-01 21:44:29 +0530

    commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
-const path = require('path');
 const Event = require('./schema.js');
 const Eventdes = require('./schemaEvent.js');
 const Cart = require('./myevents.js');
