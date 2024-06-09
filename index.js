6b5b67ba35899b674eececfc0be34d9f3fb8d470
amruta.ks <amruta.ks2022cse@sece.ac.in>
2024-06-09 21:47:30 +0530

    commit

--- index.js
+++ index.js
@@ -20,12 +20,13 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
+// Multer storage configuration
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname); // Use only the original filename
+        cb(null, `${Date.now()}_${file.originalname}`);
     }
 });
 
